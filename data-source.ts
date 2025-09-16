9b925ac10bbb0c1d962f217648274f982f1975fc
Abdulrahman Alassaf. <163548732+abdulrahman-3sf@users.noreply.github.com>
2025-09-16 22:33:40 +0300

    correcting the data-source to make migrations successfully

--- data-source.ts
+++ data-source.ts
@@ -8,11 +8,13 @@ const configService = new ConfigService();
 
 export default new DataSource({
     type: 'postgres',
-    host: configService.get('DB_HOST'),
+    host: configService.get('DB_HOST'), // or use process.env.DB_HOST
     port: +configService.get<number>('DB_PORT', 5432),
     username: configService.get('DB_USER'),
     password: configService.get('DB_PASS'),
     database: configService.get('DB_NAME'),
-    entities: [User],
-    migrations: ['dist/migrations/*.js'],
+    entities: ['src/**/*.entity.{ts,js}'],
+    migrations: ['src/migrations/*.{ts,js}'],
+    synchronize: false,
+    logging: true
 });
