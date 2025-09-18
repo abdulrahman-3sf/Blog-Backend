eabe721eb0f8c23db643c3d2b8dba61ca360dd30
Abdulrahman Alassaf. <163548732+abdulrahman-3sf@users.noreply.github.com>
2025-09-18 19:45:06 +0300

    update CreatePostDto and UpdatePostDto

--- src/posts/dto/create-post.dto.ts
+++ src/posts/dto/create-post.dto.ts
@@ -1,10 +1,13 @@
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty, IsString, MaxLength } from "class-validator";
 import { Column } from "typeorm";
 
 export class CreatePostDto {
     @IsNotEmpty({ message: 'Title is required' })
+    @IsString()
+    @MaxLength(200, { message: 'Title must be shorter than 200 characters' })
     title: string;
 
     @IsNotEmpty({ message: 'Body is required' })
+    @IsString()
     body: string;
 }
--- src/posts/dto/update-post.dto.ts
+++ src/posts/dto/update-post.dto.ts
@@ -1,7 +1,16 @@
+import { IsOptional, IsString, MaxLength } from "class-validator";
+
 export class UpdatePostDto {
+    @IsOptional()
+    @IsString()
+    @MaxLength(200, { message: 'Title must be shorter than 200 characters' })
     title?: string;
 
+    @IsOptional()
+    @IsString()
     body?: string;
 
+    @IsOptional()
+    @IsString()
     published?: boolean;
 }
