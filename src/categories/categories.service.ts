137cfec8841792e65f8a0f909665270b13673241
Abdulrahman Alassaf. <163548732+abdulrahman-3sf@users.noreply.github.com>
2025-10-03 00:32:15 +0300

    add CreateCategoryDto

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Category } from './entities/category.entity';
+import { Repository } from 'typeorm';
+import { CreateCategoryDto } from './dto/create-category.dto';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+    constructor(@InjectRepository(Category) private readonly categoryRepository: Repository<Category>) {}
+
+    create(createCategoryDto: CreateCategoryDto): Promise<Category> {
+        
+    }
+}
---
+++ src/categories/dto/create-category.dto.ts
@@ -0,0 +1,25 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { Transform } from "class-transformer";
+import { IsNotEmpty, IsString, MaxLength } from "class-validator";
+
+export class CreateCategoryDto {
+    @ApiProperty({example: 'tech'})
+    @Transform(({ value }) => typeof value === 'string' ? value.trim() : value)
+    @IsNotEmpty({message: 'Name is required'})
+    @IsString()
+    @MaxLength(80, {message: 'Name must be shorter than 80 characters'})
+    name: string;
+
+    @ApiProperty({example: 'tech'})
+    @Transform(({ value }) => typeof value === 'string' ? value.trim() : value)
+    @IsNotEmpty({message: 'Slug is required'})
+    @IsString()
+    @MaxLength(100, {message: 'Slug must be shorted than 100 characters'})
+    slug: string;
+
+    @ApiProperty({example: 'tech is about ...'})
+    @Transform(({ value }) => typeof value === 'string' ? value.trim() : value)
+    @IsString()
+    @MaxLength(300, {message: 'Description must be shorted than 300 characters'})
+    description: string;
+}
