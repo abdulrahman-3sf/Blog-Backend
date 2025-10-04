ed687d439ef74431fead1561dbd22399538df9f4
Abdulrahman Alassaf. <163548732+abdulrahman-3sf@users.noreply.github.com>
2025-10-04 23:37:42 +0300

    add create category api

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,5 +1,10 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query, UseGuards } from '@nestjs/common';
 import { CategoriesService } from './categories.service';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
+import { Roles } from 'src/auth/decorators/roles.decorator';
+import { UserRole } from 'src/users/entities/user.entity';
 
 @Controller('categories')
 export class CategoriesController {
@@ -9,5 +14,12 @@ export class CategoriesController {
     async list(@Query('withCounts') withCounts?: string) {
         const flag = (withCounts == 'true') || (withCounts == 'yes') || (withCounts == '1');
         return this.categoriesRepository.findAll({withCounts: flag});
+    }
+
+    @Post()
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles(UserRole.ADMIN)
+    async create(@Body() createCategoryDto: CreateCategoryDto) {
+        return await this.categoriesRepository.create(createCategoryDto);
     }
 }
