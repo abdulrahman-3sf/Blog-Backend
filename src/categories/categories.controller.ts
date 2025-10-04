dfdf9541db7a996c272194071abf783cb9c95457
Abdulrahman Alassaf. <163548732+abdulrahman-3sf@users.noreply.github.com>
2025-10-04 23:30:20 +0300

    add list categories api

--- src/categories/categories.controller.ts
+++ src/categories/categories.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { CategoriesService } from './categories.service';
 
 @Controller('categories')
-export class CategoriesController {}
+export class CategoriesController {
+    constructor(private readonly categoriesRepository: CategoriesService) {}
+
+    @Get()
+    async list(@Query('withCounts') withCounts?: string) {
+        const flag = (withCounts == 'true') || (withCounts == 'yes') || (withCounts == '1');
+        return this.categoriesRepository.findAll({withCounts: flag});
+    }
+}
